34ce8320e94213e420c14bf56917c7f2e8aaf440
Artem Medeusheyev <artem.medeu@gmail.com>
2013-11-28 12:40:41 +0600

    tests are passing

--- mkdirp.go
+++ mkdirp.go
@@ -2,17 +2,84 @@ package mkdirp
 
 import (
 	"regexp"
-	"strings"
 )
 
-var validPath = regexp.MustCompile(`^.+/$`)
+var sep = regexp.MustCompile(`(\}/){1}|([\{\},]){1}`)
+
+var (
+	startSubtree = "{"
+	eachSubtree  = "}/"
+	endSubtree   = "}"
+	sibling      = ","
+)
 
 type dirTree struct {
-	name string
-	subs []*dirTree
+	name     string
+	branches []*dirTree
+	parent   *dirTree
 }
 
-func mk(path string) []string {
-	trimmed := strings.Trim(path, "/ ")
-	return []string{path}
+func (t *dirTree) newBranch() (branch *dirTree) {
+	branch = &dirTree{branches: []*dirTree{}, parent: t}
+	t.branches = append(t.branches, branch)
+	return
 }
+
+func build(data string, tree *dirTree) {
+	if len(data) <= 1 {
+		return
+	}
+	l := sep.FindStringIndex(data)
+	if l != nil {
+		br := tree.newBranch()
+		br.name = data[:l[0]]
+		next := data[l[1]:]
+		if s := string(data[l[0]:l[1]]); s == startSubtree {
+			build(next, br)
+		} else if s == eachSubtree {
+			for _, b := range tree.branches {
+				build(next, b)
+			}
+		} else if s == endSubtree {
+			build(next, tree.parent)
+		} else { // sibling
+			build(next, tree)
+		}
+	} else {
+		br := tree.newBranch()
+		br.name = data
+	}
+}
+
+func mkTree(command string) *dirTree {
+	tree := &dirTree{branches: []*dirTree{}}
+	build(command, tree)
+
+	// result := []string{}
+	// addPath := func(p string) {
+	// 	result = append(result, p)
+	// }
+	return tree.branches[0]
+}
+
+// func travel(tree *dirTree, fn func(string)) {
+// 	if len(tree.branches) == 0 {
+// 		p := []string{}
+// 		elem := tree
+// 		for {
+// 			if elem == nil {
+// 				break
+// 			}
+// 			tmp := make([]string, len(p)+1)
+// 			tmp[0] = elem.name
+// 			copy(tmp, p)
+// 			p = tmp
+// 			elem = elem.parent
+// 		}
+// 		fn(strings.Join(p, ""))
+// 		return
+// 	}
+// 	for _, br := range tree.branches {
+// 		travel(br, fn)
+// 	}
+// }
--- mkdirp_test.go
+++ mkdirp_test.go
@@ -8,19 +8,51 @@ import (
 var c = Convey
 
 func TestMkdirp(t *testing.T) {
-	c("mk func should", t, func() {
-		c("return list of paths to create", func() {
-			result := mk("/Users/me/somefolder")
-
-			So(len(result), ShouldEqual, 1)
-			So(result[0], ShouldEqual, "/Users/me/somefolder")
+	c("mkTree should", t, func() {
+		c("return tree from straight path", func() {
+			root := mkTree("Users/me/somefolder")
+			So(root, ShouldNotBeNil)
+			So(len(root.branches), ShouldEqual, 0)
+			So(root.name, ShouldEqual, "Users/me/somefolder")
 		})
-		c("support creating of trees", func() {
-			result := mk("/Users/me/{somefolder,somefolder2}")
+		c("return tree with subtrees", func() {
+			root := mkTree("Users/me/{somefolder,somefolder2}/test/{data1,data2}")
 
-			So(len(result), ShouldEqual, 2)
-			So(result[0], ShouldEqual, "/Users/me/somefolder")
-			So(result[1], ShouldEqual, "/Users/me/somefolder2")
+			So(root.name, ShouldEqual, "Users/me/")
+			subtree := root.branches
+			So(len(subtree), ShouldEqual, 2)
+			So(subtree[0].name, ShouldEqual, "somefolder")
+			So(subtree[1].name, ShouldEqual, "somefolder2")
+
+			sstree := subtree[0]
+			sstree2 := subtree[1]
+			So(len(sstree.branches), ShouldEqual, 1)
+			So(sstree.branches[0].name, ShouldEqual, "test/")
+			So(len(sstree2.branches), ShouldEqual, 1)
+			So(sstree2.branches[0].name, ShouldEqual, "test/")
+
+			test := sstree.branches[0]
+			So(len(test.branches), ShouldEqual, 2)
+			So(test.branches[0].name, ShouldEqual, "data1")
+			So(test.branches[1].name, ShouldEqual, "data2")
+
 		})
 	})
+	// c("mk func should", t, func() {
+	// 	c("return list of paths to create", func() {
+	// 		result := mk("/Users/me/somefolder")
+
+	// 		So(len(result), ShouldEqual, 1)
+	// 		So(result[0], ShouldEqual, "/Users/me/somefolder")
+	// 	})
+	// 	c("support creating of trees", func() {
+	// 		result := mk("tmpdir/{trunk/sources/{includes,docs},branches,tags}")
+	// 		fmt.Println(result)
+	// 		So(len(result), ShouldEqual, 4)
+	// 		So(result[0], ShouldEqual, "tmpdir/trunk/sources/includes")
+	// 		So(result[1], ShouldEqual, "tmpdir/trunk/sources/docs")
+	// 		So(result[2], ShouldEqual, "tmpdir/trunk/branches")
+	// 		So(result[3], ShouldEqual, "tmpdir/trunk/tags")
+	// 	})
+	// })
 }
